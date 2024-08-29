d200d58ba2cec950d1a357171c10c55e54d592ba
Abdul Vahab A A <abdulvahabaa916@gmail.com>
2024-08-29 21:50:20 +0530

    refactor:user data funcion corrected

--- controllers/userData.mjs
+++ controllers/userData.mjs
@@ -1,19 +1,21 @@
+import collecion from "../connection/collecion.mjs";
 import connectToDatabase from "../connection/db.mjs";
 
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
+    console.log(id)
+    const db = await connectToDatabase("PERSONAL");
+    const result = await  db
+      .collection(collecion.USER_COLLECTIION)
+      .find({ userId: id })
+      .toArray();
 
-    // const result = await pool.query(
-    //   "SELECT id, userid, name, email, created_at FROM public.users WHERE userid = $1",
-    //   [id]
-    // );
-
-    if (result.rows.length === 0) {
+    if (result.length === 0) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result);
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ message: "Internal server error" });
--- routes/users.mjs
+++ routes/users.mjs
@@ -5,9 +5,8 @@ import { getUser } from "../controllers/userData.mjs";
 
 const userRoutes = express.Router({ mergeParams: true });
 
-// userRoutes.get("/:id", getUser);
+userRoutes.get("/:id", getUser);
 
 // userRoutes.get("/:id", verifyToken, getUser);
-
 
 export default userRoutes;
